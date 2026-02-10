e9191245b48936092091bb055aa9eccafda7e08b
Alix Bott <bott.alix@gmail.com>
2026-02-10 19:17:55 +0000

    fix occasional failure to start (#10)

--- workload/bin/init/main.go
+++ workload/bin/init/main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"context"
 	"fmt"
+	"net"
 	"os"
 	"os/signal"
 	"syscall"
@@ -11,11 +12,68 @@ import (
 	"github.com/antithesishq/antithesis-sdk-go/lifecycle"
 	"github.com/formancehq/dst/workload/internal"
 	"github.com/formancehq/formance-sdk-go/v3/pkg/models/operations"
+
+	v1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/apimachinery/pkg/apis/meta/v1/unstructured"
+	"k8s.io/apimachinery/pkg/runtime/schema"
+	"k8s.io/client-go/dynamic"
+
+	"k8s.io/client-go/tools/clientcmd"
 )
+
+func waitForPostgres() {
+	for {
+		conn, err := net.DialTimeout("tcp", "postgres:5432", 500*time.Millisecond)
+		if err != nil {
+			time.Sleep(time.Second)
+			continue
+		}
+		_ = conn.Close()
+		return
+	}
+}
+
+func setupStack() {
+	config, err := clientcmd.BuildConfigFromFlags("", "")
+	if err != nil {
+		panic(err)
+	}
+
+	dyn, err := dynamic.NewForConfig(config)
+	if err != nil {
+		panic(err)
+	}
+
+	gvr := schema.GroupVersionResource{
+		Group:    "formance.com",
+		Version:  "v1beta1",
+		Resource: "stacks",
+	}
+
+	_, err = dyn.Resource(gvr).Create(context.Background(), &unstructured.Unstructured{
+		Object: map[string]any{
+			"apiVersion": "formance.com/v1beta1",
+			"kind":       "Stack",
+			"metadata": map[string]any{
+				"name": "stack0",
+			},
+			"spec": map[string]any{
+				"versionsFromFile": "v2.0",
+			},
+		},
+	}, v1.CreateOptions{})
+	if err != nil {
+		panic(err)
+	}
+}
 
 func main() {
 	ctx := context.Background()
 	client := internal.NewClient()
+
+	waitForPostgres()
+
+	setupStack()
 
 	for {
 		time.Sleep(time.Second)
